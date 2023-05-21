275556f43ef5463b9c3295f4368f706f3b29048e
Andrii Sadovyi <sadoviya@gmail.com>
2023-05-21 21:13:19 +0300

    task 2 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList.add("item");
+  return element;
+});
+
+ingredientsRef.append(...elements);
