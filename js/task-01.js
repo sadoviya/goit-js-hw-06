ef1cc86188efabbbf127dcd6971988f753dba73c
Andrii Sadovyi <sadoviya@gmail.com>
2023-05-21 18:50:14 +0300

    homework 1

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const targetItemsRef = document.querySelectorAll(".item");
+console.log("Number of categories:", targetItemsRef.length);
+const targetUlRef = targetItemRef.parentNode;
+console.log(targetUlRef);
