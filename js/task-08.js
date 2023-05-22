0156dd9dc0af2dbf055cd6ad19ccac00f6787765
Andrii Sadovyi <sadoviya@gmail.com>
2023-05-22 19:16:07 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені (All fields must be filled).");
+  }
+
+  const user = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (email.value !== "" && password.value !== "") {
+    console.log(user);
+    event.currentTarget.reset();
+  }
+});
